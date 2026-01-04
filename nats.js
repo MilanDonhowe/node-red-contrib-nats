29b5d98427474faa8ec60f1110acaa04e2053ea5
Milan Donhowe <milanaugust@gmail.com>
2026-01-04 14:35:03 -0800

    added string output type checkbox

--- nats.js
+++ nats.js
@@ -9,7 +9,13 @@ module.exports = function(RED) {
   }
   
   function formatNatsError(err){
-    return `${err.code ?? "unknown error code (internal node error)"}: ${err.input ?? "unknown user input (internal node error)"}`
+    if (err.code && err.input){
+      return `${err.code}: ${err.input}`
+    }
+    if (err.code){
+      return `${err}`
+    }
+    return "unknown internal nats error"
   }
 
   /* subscription node */
@@ -28,6 +34,8 @@ module.exports = function(RED) {
     this.user = config.user;
     this.pass = config.pass;
     this.subject = config.subject;
+    this.outputAsString = config.outputAsString;
+
     
 
     let natsConRef = null;
@@ -38,36 +46,54 @@ module.exports = function(RED) {
       .then((natsConnection) => {
         // hacky reference
         natsConRef = natsConnection;
-        //const subject = this.subject;
-        node.status({"fill": "green", "shape": "dot", "text": "connected to broker"});
+        
+        node.status({"fill": "green", "shape": "dot", "text": `connected to broker`});
 
         // sid is an async iterator
         const sid = natsConnection.subscribe(this.subject);
         (async () => {
           for await (const msg of sid){
-            node.send({"payload":msg.data, "subject": msg.subject});
+            // msg.data will default to a Buffer type
+            let data = msg.data;
+            // msg.string should be the UTF-8 encoded payload
+            if (node.outputAsString){
+              try {
+                data = msg.string();
+              } catch (error){
+                // TODO: test this out
+                // stole idea here from https://github.com/node-red/node-red/blob/6a75a084adc159de1be047e554b7463c306692a9/packages/node_modules/%40node-red/nodes/core/network/10-mqtt.js#L1323C27-L1323C62
+                // since we don't accept input messages, not clear from docs on how to report errors to runtime
+                node.error(RED._("nats.errors.utf-encoding"))
+              }
+            }
+            node.send({"payload":data, "subject": msg.subject});
           }
+          // at this point, our async iterator is closed so we clear node status here
+          node.status({})
         })();
       })
       // notify node-red editor & user that nats broker connection failed
       .catch((err) => {
         let errMsg = formatNatsError(err);
-        this.error(errMsg);
+        node.error(errMsg);
         node.status({"fill": "red", "shape": "ring", "text": errMsg});
       })
     
     // destructor for node
-    this.on('close', function(){
-      // clear status
-      node.status({});
+    node.on('close', function(done){
       if (natsConRef !== null){
         natsConRef.drain().then(() => {
           // happy flow; do nothing we chill
+          done();
         }).catch(err => {
           // huh, we should log this.  Not sure how this would otherwise fail
           let errMsg = formatNatsError(err);
           node.warn("error closing nats con: " + errMsg);
+          done();
         })
+      } else {
+        // IMPORTANT! if no connection exists immediately signal we're done to node-red
+        done();
       }
     })
   }
@@ -112,7 +138,7 @@ module.exports = function(RED) {
       this.subject = msg.payload.subject || config.subject;
       this.message = msg.payload.message || config.message;
 
-      // so we essentially drop messages iff there's no 
+      // so we essentially drop messages if there's no active connection
       if (this.subject && this.message){
         if (natsConRef !== null) {
           natsConRef.publish(this.subject, this.message);
